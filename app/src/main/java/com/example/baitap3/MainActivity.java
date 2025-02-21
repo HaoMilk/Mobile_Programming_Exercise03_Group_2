faf86440688c2b23da2fd045ed9b4d9e197c77be
HaoNguyen <haoytp@gmail.com>
2025-02-21 08:24:27 +0700

    "doi hinh nen khi load app va bam swicth control"

--- app/src/main/java/com/example/baitap3/MainActivity.java
+++ app/src/main/java/com/example/baitap3/MainActivity.java
@@ -8,17 +8,47 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.widget.CompoundButton;
+import android.widget.RelativeLayout;
+import android.widget.Switch;
+
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RelativeLayout layout;
+    private Switch switchBackground;
+    private int[] backgroundImages = {
+            R.drawable.background1,
+            R.drawable.background2,
+            R.drawable.bottom_btn1,
+            R.drawable.bottom_btn2
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        layout = findViewById(R.id.layout);
+        switchBackground = findViewById(R.id.switchBackground);
+
+        changeBackground();
+
+        switchBackground.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    changeBackground();
+                }
+            }
         });
     }
+
+    private void changeBackground() {
+        Random random = new Random();
+        int randomIndex = random.nextInt(backgroundImages.length);
+        layout.setBackgroundResource(backgroundImages[randomIndex]);
+    }
 }
+
